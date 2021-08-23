dcf853e59b4d909569602a8fd179865fe814f27b
Alessandro Foschi <alessandro.foschi5@gmail.com>
2021-08-23 18:54:39 +0200

    Minor code fixes

--- composer/projects/symfony-web-toolkit-skeleton/src/Entity/User.php
+++ composer/projects/symfony-web-toolkit-skeleton/src/Entity/User.php
@@ -6,9 +6,7 @@ use App\Repository\UserRepository;
 use Doctrine\ORM\Mapping as ORM;
 use Mep\WebToolkitBundle\Contract\Entity\AbstractUser;
 
-/**
- * @ORM\Entity(repositoryClass=UserRepository::class)
- */
+#[ORM\Entity(repositoryClass: UserRepository::class)]
 class User extends AbstractUser
 {
 }
