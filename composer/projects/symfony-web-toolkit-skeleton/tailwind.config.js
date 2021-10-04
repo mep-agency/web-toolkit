dc8bf719389007bdfd6de36e5f6914a00d90363d
Zekel97 <szandreas97@gmail.com>
2021-10-04 20:48:08 +0200

    Implemented tailwind in the skeleton

---
+++ composer/projects/symfony-web-toolkit-skeleton/tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    mode: 'jit',
+    purge: [
+        './templates/**/*.html.twig',
+    ],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {},
+    },
+    variants: {},
+    plugins: [],
+}
