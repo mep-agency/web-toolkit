0d56200cc8acd6941f2f579fbeb7510d56b08b1b
Zekel97 <szandreas97@gmail.com>
2021-12-22 12:41:52 +0100

    Updating Webpack Encore to 1.7.0 and removing Stimulus dependencies

--- composer/projects/symfony-web-toolkit-skeleton/webpack.config.js
+++ composer/projects/symfony-web-toolkit-skeleton/webpack.config.js
@@ -30,7 +30,7 @@ Encore
     })*/
 
     // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
-    .enableStimulusBridge('./assets/controllers.json')
+    //.enableStimulusBridge('./assets/controllers.json')
 
     // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
     .splitEntryChunks()
