8e8453dbca893075af8b19e99f3d087a0822a1c5
Alessandro Foschi <alessandro.foschi5@gmail.com>
2022-02-09 18:06:01 +0100

    Skeleton updates

--- composer/projects/symfony-web-toolkit-skeleton/src/Controller/Admin/DashboardController.php
+++ composer/projects/symfony-web-toolkit-skeleton/src/Controller/Admin/DashboardController.php
@@ -9,12 +9,24 @@ use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Contracts\Menu\MenuItemInterface;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
 use Iterator;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 #[IsGranted('ROLE_USER')]
 class DashboardController extends AbstractDashboardController
 {
+    #[Route('/admin')]
+    public function index(): Response
+    {
+        /** @var AdminUrlGenerator $adminUrlGenerator */
+        $adminUrlGenerator = $this->container->get(AdminUrlGenerator::class);
+
+        return $this->redirect($adminUrlGenerator->setController(UserCrudController::class)->generateUrl());
+    }
+
     public function configureDashboard(): Dashboard
     {
         return Dashboard::new()
@@ -27,8 +39,7 @@ class DashboardController extends AbstractDashboardController
      */
     public function configureMenuItems(): iterable
     {
-        yield MenuItem::linktoDashboard('Dashboard', 'fa fa-home');
-//        yield MenuItem::linkToCrud('Article', 'fas fa-edit', Article::class);
+        // yield MenuItem::linktoDashboard('Dashboard', 'fa fa-home');
         yield MenuItem::linkToCrud('User', 'fas fa-user', User::class);
     }
 }
--- composer/projects/symfony-web-toolkit-skeleton/src/Controller/Admin/UserCrudController.php
+++ composer/projects/symfony-web-toolkit-skeleton/src/Controller/Admin/UserCrudController.php
@@ -8,7 +8,6 @@ use App\Entity\User;
 use EasyCorp\Bundle\EasyAdminBundle\Contracts\Field\FieldInterface;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 
@@ -26,7 +25,6 @@ class UserCrudController extends AbstractCrudController
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id')->onlyOnIndex(),
             TextField::new('email'),
             ChoiceField::new('roles')
                 ->setChoices([
