fd714342b18ffbc4b57869c3d958265136a1dc77
Zekel97 <szandreas97@gmail.com>
2022-07-21 15:39:52 +0200

    Updating npm package dependencies

--- composer/projects/symfony-web-toolkit-skeleton/assets/scripts/main.ts
+++ composer/projects/symfony-web-toolkit-skeleton/assets/scripts/main.ts
@@ -10,8 +10,5 @@ import '../styles/main.scss';
 
 import ConsentIndex from '@mep-agency/web-toolkit-front-end/scripts/ConsentManagement/BuiltInSolution/ConsentIndex';
 
-// The following line imports some useful tools with a default configuration
-// import '@mep-agency/web-toolkit-front-end';
-
 // The following line initializes the built-in consent manager with default styling and settings.
 ConsentIndex({ defaultStyle: true, defaultIcon: true }, 30000);
