ea8abda571065bc17a22ee663b678076c070b63a
Alessandro Foschi <alessandro.foschi5@gmail.com>
2023-01-30 17:32:13 +0100

    Adding missing comments

--- composer/projects/symfony-web-toolkit-skeleton/rector.php
+++ composer/projects/symfony-web-toolkit-skeleton/rector.php
@@ -11,16 +11,22 @@ use Rector\Symfony\Set\SymfonySetList;
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->parallel();
 
+    // Set target PHP version
     $rectorConfig->phpVersion(PhpVersion::PHP_81);
 
+    // Sources
     $rectorConfig->paths([__DIR__.'/src', __DIR__.'/tests', __DIR__.'/ecs.php', __DIR__.'/rector.php']);
 
+    // Skip some stuff
     $rectorConfig->skip([__DIR__.'/src/Kernel.php', __DIR__.'/tests/bootstrap.php']);
 
+    // Ensure the Web Toolkit classes are always loaded correctly
     $rectorConfig->autoloadPaths([__DIR__.'/vendor/mep-agency/web-toolkit-bundle/src']);
 
+    // Enable Symfony support
     $rectorConfig->symfonyContainerXml(__DIR__.'/var/cache/dev/App_KernelDevDebugContainer.xml');
 
+    // Define what rule sets will be applied
     $rectorConfig->sets([
         SetList::PHP_81,
         SetList::DEAD_CODE,
@@ -32,5 +38,6 @@ return static function (RectorConfig $rectorConfig): void {
         SetList::TYPE_DECLARATION,
     ]);
 
+    // Custom configuration
     $rectorConfig->importNames();
 };
